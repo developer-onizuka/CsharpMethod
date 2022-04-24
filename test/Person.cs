05c5b85d12e52f8627a1af0b0609114ec177b130
vagrant <vagrant@mvc>
2022-04-24 11:23:44 +0000

    2022-04-24

---
+++ test/Person.cs
@@ -0,0 +1,10 @@
+public class Person
+{
+    public string Name;
+    public int Age;
+    
+    public void ActualAge(int y)
+    {
+        this.Age = this.Age + y;
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Person p = new Person();
+	p.Name = "developer-onizuka";
+	p.Age = 25;
+        p.ActualAge(10);
+	Console.WriteLine("Name: " + p.Name);
+	Console.WriteLine("Age: " + p.Age);
+    }
+}
+
